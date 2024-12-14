10f54bab6d29b25fb40d9dd6e3005c6fda245dca
Obaid Ramadan <obiedramadan0@gmail.com>
2024-12-15 01:49:02 +0200

    make nav bar and landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SigninComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
